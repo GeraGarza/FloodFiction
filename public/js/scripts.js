f94ddf5f252ac29943aa6d48efa1e79bebbb611c
Gera Garza <josegerag@hotmail.com>
2020-03-01 05:01:14 -0600

    base working

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,52 +1,14 @@
 
-window.onload = function () {
-
-  FindMe();
-  function geoFindMe() {
-
-    const status = document.querySelector('#status');
-    const mapLink = document.querySelector('#map-link');
-
-    mapLink.href = '';
-    mapLink.textContent = '';
 
 
-    function success(position) {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
-
-      status.textContent = '';
-      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-    }
-
-    function error() {
-      status.textContent = 'Unable to retrieve your location';
-    }
-
-    if (!navigator.geolocation) {
-      status.textContent = 'Geolocation is not supported by your browser';
-    } else {
-      status.textContent = 'Locating…';
-      navigator.geolocation.getCurrentPosition(success, error);
-    }
-
-  }
-
-  document.querySelector('#find-me').addEventListener('click', geoFindMe);
-}
-
-function FindMe() {
+function FindMe(usrTxt = "gg") {
   if ("geolocation" in navigator) {
     console.log('geo is available.')
     navigator.geolocation.getCurrentPosition(async postition => {
-      console.log(postition)
       const lat = postition.coords.latitude;
       const long = postition.coords.longitude;
-      document.getElementById('latitude').textContent = lat;
-      document.getElementById('longitude').textContent = long;
-
-      const data = { lat, long };
+     
+      const data = { lat, long, usrTxt};
       const options = {
         method: 'POST',
         body: JSON.stringify(data),
@@ -68,42 +30,67 @@ function FindMe() {
 
 
 
+var totalTiles = 0;
 
+async function GetData() {
 
-async function GetData(a) {
+  // var gg = document.getElementById("DBid");
+  const DB = document.querySelector('#DBid');
 
-  var gg = document.getElementById("DBid");
   // can be same as other api since this is with get, not post
   const response = await fetch('/api');
   const data = await response.json();
+
   // console.log(data)  
   var styles = "border-radius:25px; background:rgb(100, 30, 200); color:white; padding: 12px;";
+ 
+  let subsWrapper = document.getElementById("temp");
+  if(subsWrapper) {
+    subsWrapper.remove();
+  }
+
+  const template = document.createElement('div');
+  template.id = "temp";
+  template.style.cssText = "background-color: rgb(120, 40, 220); border-radius: 25px;";
 
   for (item of data) {
     const root = document.createElement('div');
     root.style.cssText = styles;
 
     const mood = document.createElement('div');
-    const geo = document.createElement('div');
-    const date = document.createElement('div');
+    const metadata = document.createElement('div');
+    metadata.style.fontSize = '9px';
+  
+    const usrTxt = document.createElement('div');
     const hor = document.createElement('hr');
 
-
     mood.textContent = `mood: ${item.status}`;
-    geo.textContent = `${item.lat} , ${item.long}`;
     const dateString = new Date(item.timestamp).toLocaleString();
-    date.textContent = dateString;
-    root.append(mood, geo, date);
-    gg.append(root, hor);
-   
+    metadata.textContent = `Location: ${item.lat.toFixed(2)} , ${item.long.toFixed(2)} | Time: ${dateString}`;
+    usrTxt.textContent = `usrTxt: ${item.usrTxt}`
+    root.append(mood, usrTxt, metadata);
+    template.append(root, hor);
+    DB.append(template);
+    
+    totalTiles ++;
+  }
+  console.log(data.length)
+  console.log(totalTiles)
+}
+
+
+function DeleteDB(){
+  let subsWrapper = document.getElementById("temp");
+  if(subsWrapper) {
+    subsWrapper.remove();
   }
 }
 
 
-
-
-
 function SubmitTxt() {
-  var x = document.getElementById("myInput").value;
-  document.getElementById("txtOut").innerHTML = x;
+  var usrTxt = document.getElementById("myInput").value;
+  document.getElementById("txtOut").textContent = usrTxt;
+  FindMe(usrTxt);
+
+
 }
